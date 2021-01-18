3925ae06509dda1606d0bfa27dadd671bb21b7d4
José Ruiz <7784299-joseruizdev@users.noreply.gitlab.com>
2021-01-18 03:38:34 +0100

    Create an UI element to show the user score

--- environment/ObstacleSpawner.gd
+++ environment/ObstacleSpawner.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+signal obstacle_created(obs)
+
 onready var timer = $Timer
 
 var Obstacle = preload("res://environment/Obstacle.tscn")
@@ -14,3 +16,10 @@ func spawn_obstacle():
 	var obstacle = Obstacle.instance()
 	add_child(obstacle)
 	obstacle.position.y = randi() % 400 + 150
+	emit_signal("obstacle_created", obstacle)
+
+func start():
+	timer.start()
+
+func stop():
+	timer.stop()
